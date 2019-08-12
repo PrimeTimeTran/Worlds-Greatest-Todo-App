4196cf03fd4fad9c59bf69e8e9b1081382a7d236
Loi Tran <datloiboi@gmail.com>
2019-08-12 21:19:45 +0700

    Add transition group

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { CSSTransitionGroup } from "react-transition-group"; // ES6
 
 import TodoItem from "./TodoItem";
 import TodoPrompt from "./TodoPrompt";
@@ -15,19 +16,25 @@ const TodoList = ({
     <div className="TodoContainer">
       {loading && <div className="loader" />}
       <TodoPrompt todoList={todos} currentUser={currentUser} />
-      {todos.map((todo, idx) => {
-        return (
-          <TodoItem
-            idx={idx}
-            todo={todo}
-            id={todo.id}
-            key={todo.id}
-            onToggleTodo={onToggleTodo}
-            submitEditTodo={submitEditTodo}
-            onDeleteTodo={id => onDeleteTodo(id)}
-          />
-        );
-      })}
+      <CSSTransitionGroup
+        transitionName="example"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}
+      >
+        {todos.map((todo, idx) => {
+          return (
+            <TodoItem
+              idx={idx}
+              todo={todo}
+              id={todo.id}
+              key={todo.id}
+              onToggleTodo={onToggleTodo}
+              submitEditTodo={submitEditTodo}
+              onDeleteTodo={id => onDeleteTodo(id)}
+            />
+          );
+        })}
+      </CSSTransitionGroup>
     </div>
   );
 };
